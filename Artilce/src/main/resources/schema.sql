f483345f07e51ebc592802af2e3d2742e9dfd755
deeptikalegore <deeptikalegore@gmail.com>
2022-07-05 22:05:24 +0530

    d

--- Artilce/src/main/resources/schema.sql
+++ Artilce/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS article;  
 create table article(aid int NOT NULL AUTO_INCREMENT,uid int,title varchar(100),type varchar(100),
-content longtext(2000),image text(500),date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,likes int,dislikes int,primary key(aid));
+content longtext(2000),image text(500),date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,likes int,primary key(aid));
 
 DROP TABLE IF EXISTS comment;  
 create table comment(cid int NOT NULL AUTO_INCREMENT,aid int,uid int,comment varchar(100),date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,primary key(cid));
